2260f5ad2c965ed5dd641762407ad509b48d093e
Abdullah Farid <99048961+Zaz7@users.noreply.github.com>
2022-12-25 14:28:36 +0300

    Delete find-pivot-index.py

--- find-pivot-index.py
+++
@@ -1,9 +0,0 @@
-otal= sum(nums)
-        left_sum= 0 
-        for i in range(len(nums)):
-            right_sum = Total-nums[i]-left_sum
-            if left_sum == right_sum:
-                return i
-            left_sum+=nums[i]
-        return -1
-            
