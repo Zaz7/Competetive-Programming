be9691ed47f099ea2ac52511fbb4f57abcb11f11
Abdullah Farid <99048961+Zaz7@users.noreply.github.com>
2023-03-31 15:40:43 +0300

    Time: 19 ms (99.74%), Space: 13.8 MB (95.61%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.py
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.py
@@ -4,10 +4,8 @@ class Solution:
         left = 0
         right = k
         n = str(num)
-        
         counter = 0
-        start = 0
-        end = k
+        
         
         
         for iteration in range (0 , len(n)-k+1) :
@@ -15,21 +13,17 @@ class Solution:
             if int(n[left: right]) ==0:  
                 left += 1
                 right += 1
-                #start += 1
-                #end += 1
-                    
+               
             elif num % int(n[left: right]) == 0 :
                 counter += 1
                 left += 1
                 right += 1
-                #start += 1
-                #end += 1
+                
             
             else:
                 left += 1
                 right += 1
-                #start += 1
-                #end += 1
+                
         
         return counter
     
